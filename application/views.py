882b476a60e2a24973f850a246b9c300f564dbf2
CAN DURAN UNALDI <e2036523@ceng.metu.edu.tr>
2019-01-16 19:54:38 +0300

    Add Question

--- application/views.py
+++ application/views.py
@@ -161,7 +161,7 @@ def question_detail(request):
 def pdfcreate(request):
     question_id = request.GET.get('question_id', None)
     print(question_id)
-    if question_id != None or question_id != "":
+    if question_id != None and question_id != "":
         question_id = question_id[:-1]
     print(question_id)
     latex = request.GET.get('latex', None)
